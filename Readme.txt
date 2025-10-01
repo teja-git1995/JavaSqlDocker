5af7c6da5508b1265cc40c608f3b459dec024a4c
root <root@ip-172-31-28-197.ap-south-1.compute.internal>
2025-10-01 11:12:52 +0000

    project committed

--- Readme.txt
+++
@@ -1,2 +0,0 @@
-This is readme file
-this is my first project
---
+++ backend/src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from backend!";
+    }
+}
+
